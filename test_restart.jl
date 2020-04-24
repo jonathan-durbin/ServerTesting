a6b82b2a18b96ef57bcb538c0a9300c6d6f61e13
jonathan-durbin <jongdurbin@gmail.com>
2020-04-24 14:05:43 -0400

    Get example working

--- test_restart.jl
+++ test_restart.jl
@@ -1,34 +1,40 @@
-using HTTP
+using HTTP, JSON
 
 function getCurrentCommit(owner, repo)
     headers = Dict(
-        "Accept" => "application/vnd.github.VERSION.sha",
+        # "Accept" => "application/vnd.github.VERSION.sha",
         "Connection" => "close"
     )
     try
-        resp = HTTP.get("https://api.github.com/repos/$owner/$repo/commits/", headers)
-        return resp.body
+        resp = HTTP.get("https://api.github.com/repos/$owner/$repo/git/ref/heads/master", headers)
+        # @show resp.body
+        resp = JSON.parse(String(resp.body))
+        return resp["object"]["sha"]
     catch e
         throw(e)
     end
 end
 
-function repoUpdated(sha)
-    newsha = getCurrentCommit()
+function repoUpdated(sha, owner, repo)
+    newsha = getCurrentCommit(owner, repo)
     return sha == newsha ? false : true
 end
 
 function main()
     command = `julia $PROGRAM_FILE`
-    sha = getCurrentCommit()
+    owner = "jonathan-durbin"
+    repo = "ServerTesting"
+    v = 1
+    sha = getCurrentCommit(owner, repo)
 
     # Check whether github repo has updated
-    if !repoUpdated(sha)
+    if !repoUpdated(sha, owner, repo)
         # if not, do work
-        println("sleeping...")
-        sleep(5)
+        println("sleeping... V$v")
+        sleep(10)
     else
         # if repo is updated, re-run PROGRAM_FILE
+        println("restarting... V$v")
         atexit(() -> run(command))
         exit(0)
     end
